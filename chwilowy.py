93abed24f6bf66e8a5f6a943780294d0d6fdc1a9
mateuszwez <mateuszwez@op.pl>
2020-01-18 16:31:50 +0100

    finished test_objects.py

--- chwilowy.py
+++ chwilowy.py
@@ -1,10 +1,1 @@
-def create_file(file_path):
-    try:
-        with open(f'{file_path}.txt', 'w') as file:
-            file.write('created')
-    except FileNotFoundError as e:
-        print(e)
-    except OSError as e:
-        print(e)
-
-create_file(input())
+print(-1//2.1)
